878285808d5e39438e2ada754369f08c33387eb6
unknown <talk2uyjacob@gmail.com>
2025-07-17 18:41:11 +0100

    course update

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -35,17 +35,29 @@ exports.getAllBootCamps = asyncWrapper(async function (req, res, next) {
 
   const page = Math.abs(req.query.page) || 1;
   const limit = Math.abs(req.query.limit) || 100;
-  const skip = (page - 1) * limit;
-  console.log(page, limit, skip);
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
 
-  query = query.skip(skip).limit(limit);
+  query = query.skip(startIndex).limit(limit);
 
   const bootcamp = await query;
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = { page: page + 1, limit };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = { page: page - 1, limit };
+  }
 
   res.status(200).json({
     status: "Success",
     count: bootcamp.length,
     data: bootcamp,
+    pagination,
   });
 });
 
